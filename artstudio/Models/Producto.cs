0d14b9d25a90a30c61c3b5938f1dbefe779df5f9
Sebastian <sebastiansierra1311@gmail.com>
2024-07-11 19:30:38 -0500

    Solucion error id de categoria

--- artstudio/Models/Producto.cs
+++ artstudio/Models/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace artstudio.Models
 {
@@ -8,13 +9,14 @@ namespace artstudio.Models
         public int IdProducto { get; set; }
         public string NombreProducto { get; set; } = null!;
         public int IdCategoria { get; set; }
-        public string? Imagenes { get; set; }
+        public string Imagenes { get; set; } = null!;
         public string? DescripcionProducto { get; set; }
         public string? ListTags { get; set; }
         public int CantVendido { get; set; }
         public int? Posicion { get; set; }
         public bool? Destacado { get; set; }
 
+        [JsonIgnore]
         public virtual Categoria IdCategoriaNavigation { get; set; } = null!;
     }
 }
