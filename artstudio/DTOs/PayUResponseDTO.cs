67984ac9ad9f02448daa3c85562b65bb08f48cd4
sebastiansierra13 <johan.sierra@uptc.edu.co>
2024-10-05 23:36:44 -0500

    Arreglo en el payuresponseDTO

--- artstudio/DTOs/PayUResponseDTO.cs
+++ artstudio/DTOs/PayUResponseDTO.cs
@@ -14,8 +14,8 @@ namespace artstudio.DTOs
         public string? ReferencePol { get; set; } = null!;
         public string TransactionId { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string TrazabilityCode { get; set; } = null!;
-        public string Cus { get; set; } = null!;
+        public string? TrazabilityCode { get; set; } = null!;
+        public string? Cus { get; set; } = null!;
         public string OrderLanguage { get; set; } = null!;
         public string? Extra1 { get; set; } = null!;
         public string? Extra2 { get; set; } = null!;
@@ -31,7 +31,7 @@ namespace artstudio.DTOs
         public string Lng { get; set; } = null!;
         public string BuyerEmail { get; set; } = null!;
         public string? PseBank { get; set; } = null!;
-        public string AuthorizationCode { get; set; } = null!;
+        public string? AuthorizationCode { get; set; } = null!;
         public decimal TX_TAX { get; set; }
         public decimal TX_ADMINISTRATIVE_FEE { get; set; }
         public decimal TX_TAX_ADMINISTRATIVE_FEE { get; set; }
